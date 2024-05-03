5f877c3ddb4d3935290e24cf715501816d7f2a18
Angel Talavera <atalavera86@gmail.com>
2024-05-03 21:43:07 +0200

    Next kanji functionality

--- src/components/FromJapaneseToSpanish/FromJapaneseToSpanish.tsx
+++ src/components/FromJapaneseToSpanish/FromJapaneseToSpanish.tsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import Sensei from '../../data/main';
 import Exercises from '../Exercises/Exercises';
 import Meaning from '../Meaning/Meaning';
 
 const FromJapaneseToSpanish = () => {
-  const data = Sensei.getRandomKanji();
+  const [data, setData] = useState(Sensei.getRandomKanji());
+
+  const clickHandler = () => {
+    setData(Sensei.getRandomKanji());
+  };
 
   return <>
     <h1>{data.kanji}</h1>
     <Meaning value={data.meanings[0]} />
     <Exercises />
-    <button type="button">Siguiente Kanji</button>
+    <button className="full" type="button" onClick={clickHandler}>Siguiente Kanji</button>
   </>
 };
 
