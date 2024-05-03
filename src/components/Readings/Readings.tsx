81db552063c25ce962670380d5315e7aaa238430
Angel Talavera <atalavera86@gmail.com>
2024-05-03 21:33:12 +0200

    Readings styles

--- src/components/Readings/Readings.tsx
+++ src/components/Readings/Readings.tsx
@@ -4,12 +4,12 @@ import InputsList from '../UI/InputsList';
 const Readings = () => {
   return (
     <>
-      <div className="readings-container">
+      <div className="readings-container row">
         <InputsList title="Kunyomi" />
         <InputsList title="Onyomi" />
       </div>
-      <div>
-        <button type="button">Check</button>
+      <div className="row">
+        <button className="bnt-check half" type="button">Check</button>
       </div>
     </>
   );
